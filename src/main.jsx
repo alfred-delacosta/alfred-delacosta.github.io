715094436bd8ccc8e5bc1255ba040dc042f9e866
Alfred De La Costa <alfred_delacosta@protonmail.com>
2025-11-05 22:50:48 -0500

    Revert "Merge branch 'main' into 11-05-2025"
    This reverts commit b87a4bcf8f749ad6547f1df218459e727af0714e, reversing
    changes made to 7c6abc54fd040311fe710bc1ee32abb93ba2d794.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,47 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import App from './App'
+import Root from './routes/root'
+import Navbar from './components/navbar'
+import Index from './routes'
+import About from './routes/about'
+import Projects from './routes/projects'
+import Skills from './routes/skills'
+import Contact from './routes/contact'
 import './index.css'
-import App from './App.jsx'
+import ErrorPage from './error-page'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Index />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "projects",
+        element: <Projects />
+      },
+      {
+        path: "skills",
+        element: <Skills />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      }
+    ]
+  },
+
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
